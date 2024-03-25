df5aa7244ea07c92b6563a18d2f31c063a84c0ab
thehxdev <hossein.khosravi.ce@gmail.com>
2024-03-25 23:33:08 +0330

    testing array and string parsing

--- test/test.c
+++ test/test.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
+#include <assert.h>
 #include "../src/jacson.h"
 
 
 int main(void) {
-    char *jdata = "  \"Hello From Json!\"  ";
-    (void)jcsn_parse_json(jdata);
+    size_t i;
+    char *jdata = "[ [ \"nested array string\" ], \"This is a string inside json array\", \"Another string\" ]";
+    Jcsn_AST *ast = jcsn_parse_json(jdata);
+
+    Jcsn_JValue *root = ast->robj;
+
+    assert(root->type == J_ARRAY);
+
+    printf("\n");
+    for (i = 1; i < root->data.array->len; i++)
+        printf("%s\n", root->data.array->vals[i]->data.string);
+
+    Jcsn_JValue *nested_arr = ast->robj->data.array->vals[0];
+    for (i = 0; i < nested_arr->data.array->len; i++)
+        printf("%s\n", nested_arr->data.array->vals[i]->data.string);
+
     return 0;
 }
+
