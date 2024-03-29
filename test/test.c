7a450b17fdb501209745946fa0ca326bad546ee9
thehxdev <hossein.khosravi.ce@gmail.com>
2024-03-29 15:02:01 +0330

    get more values from AST

--- test/test.c
+++ test/test.c
@@ -7,13 +7,19 @@ int main(void) {
     Jcsn_AST *ast = jcsn_parse_json(jdata);
     if (!ast)
         return 1;
+
+    Jcsn_JValue *root = ast->root;
     printf("AST Depth = %zu\n\n", ast->depth);
 
-    Jcsn_JValue *arr = ast->root->data.object->values[0];
+    Jcsn_JValue *arr = root->data.object->values[0];
     Jcsn_JValue *inner_obj = arr->data.array->vals[0];
     printf("%s: %s\n",
            inner_obj->data.object->names[0],
            inner_obj->data.object->values[0]->data.string);
+
+    printf("%s: %ld\n",
+           root->data.object->names[1],
+           root->data.object->values[1]->data.integer);
 
     // Not implemented yet
     jcsn_ast_free(&ast);
