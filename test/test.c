197b5beded355d7c4c1e4165e95f65ac9e7cf03d
thehxdev <hossein.khosravi.ce@gmail.com>
2024-03-30 01:13:12 +0330

    updated tests for latest api changes

--- test/test.c
+++ test/test.c
@@ -4,25 +4,10 @@
 
 int main(void) {
     char *jdata = "{ \"arr\": [ { \"msg\": \"Hello From AST!\" } ], \"status\": 200, \"ok\": true, \"float_num\": 224.123, \"neg\": -90 }";
-    Jcsn_AST *ast = jcsn_parse_json(jdata);
-    if (!ast)
-        return 1;
+    Jacson *j = jcsn_parse_json(jdata);
 
-    Jcsn_JValue *root = ast->root;
-    printf("AST Depth = %zu\n\n", ast->depth);
 
-    Jcsn_JValue *arr = root->data.object->values[0];
-    Jcsn_JValue *inner_obj = arr->data.array->vals[0];
-    printf("%s: %s\n",
-           inner_obj->data.object->names[0],
-           inner_obj->data.object->values[0]->data.string);
-
-    printf("%s: %ld\n",
-           root->data.object->names[1],
-           root->data.object->values[1]->data.integer);
-
-    // Not implemented yet
-    jcsn_ast_free(&ast);
+    jcsn_free(&j);
     return 0;
 }
 
