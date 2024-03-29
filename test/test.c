278e8292e5488618df1f380ffafcf9773fa2d20e
thehxdev <hossein.khosravi.ce@gmail.com>
2024-03-30 02:30:23 +0330

    parse json files

--- test/test.c
+++ test/test.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <jacson/jacson.h>
 
+char *read_file(const char *path);
 
-int main(void) {
-    char *jdata = "{ \"arr\": [ { \"msg\": \"Hello From AST!\" } ], \"status\": 200, \"ok\": true, \"float_num\": 224.123, \"neg\": -90 }";
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <json-file-path>\n", argv[0]);
+        return 1;
+    }
+
+    char *jdata = read_file(argv[1]);
     Jacson *j = jcsn_parse_json(jdata);
 
 
     jcsn_free(&j);
+    free(jdata);
     return 0;
 }
 
+
+char *read_file(const char *path) {
+    char *content = NULL;
+    FILE *fp = fopen(path, "r");
+    if (!fp) {
+        perror("Filed to open file");
+        exit(1);
+    }
+
+    fseek(fp, SEEK_SET, SEEK_END);
+    size_t size = ftell(fp);
+    rewind(fp);
+
+    content = malloc(sizeof(char) * size);
+    fread(content, sizeof(char), size, fp);
+
+    fclose(fp);
+    return content;
+}
