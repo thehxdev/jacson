5b43e5307ca0cf0776941fa11449d06288deef42
thehxdev <hossein.khosravi.ce@gmail.com>
2025-06-20 09:11:02 +0330

    updated example program and fixed its bugs
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- test/test.c
+++ test/test.c
@@ -4,32 +4,22 @@
 #include <sys/stat.h>
 #include <jacson/jacson.h>
 
-#if defined(__linux__) || defined(__APPLE__) || defined(FreeBSD)
-    #define WITH_MMAP
-    #include <unistd.h>
-    #ifdef __APPLE__
-        #include <sys/types.h>
-    #endif
-    #include <sys/mman.h>
-#endif
-
 char *read_file(const char *path);
 
 int main(int argc, char *argv[]) {
-    //if (argc != 3) {
-    //    fprintf(stderr, "Usage: %s <json-file-path> <query>\n", argv[0]);
-    //    return 1;
-    //}
+    if (argc != 3) {
+       fprintf(stderr, "Usage: %s <json-file-path> <query>\n", argv[0]);
+       return 1;
+    }
 
-    const char *path = "F:\\projects\\c-lang\\myapps\\jacson\\test.json";
+    const char *path = argv[1];
     char *jdata = read_file(path);
     assert(jdata != NULL && "read_file returned NULL");
 
     Jacson *j = jcsn_parse_json(jdata);
     assert(j != NULL && "jcsn_parse_json returned NULL");
 
-    const char *query = "arr.[4].username";
-    //const char* query = "name";
+    const char *query = argv[2];
     Jcsn_JValue *result = jcsn_query_get(j, query);
 
     if (!result) {
@@ -84,17 +74,15 @@ char *read_file(const char *path) {
 
     if (fstat(fileno(fp), &sb) == -1)
         return NULL;
-#ifdef WITH_MMAP
-    content = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE|MAP_ANON, fileno(fp), 0);
-#else
+
     content = malloc(sb.st_size + 1);
     if (!content) {
         goto ret;
     }
+
     (void)fread(content, sizeof(char), sb.st_size, fp);
     content[sb.st_size] = 0;
 ret:
-#endif
     fclose(fp);
     return content;
 }
